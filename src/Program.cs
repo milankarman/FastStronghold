9757bf9daaa1366b232b95c19a033d00a8c17657
Milan Karman <milankarman@gmail.com>
2020-11-03 02:41:18 +0100

    Added nether coordinate tracking and portal angle calculation to find your portal easier.

--- src/Program.cs
+++ src/Program.cs
@@ -7,6 +7,7 @@ public class Program
 {
     private static bool verbose = true;
     private static List<Point> throws = new List<Point>();
+    private static Point netherPortalPoint = null;
 
     public static void Main(string[] args)
     {
@@ -56,35 +57,13 @@ public class Program
             {
                 if (clipboardString.StartsWith("/execute"))
                 {
-                    try
+                    if (clipboardString.Contains("minecraft:overworld"))
                     {
-                        throws.Add(MinecraftCommandParser.PointFromCommand(clipboardString));
-                        Text.Clear();
-
-                        if (throws.Count >= 1)
-                        {
-                            Text.Write($"Throw 1: {throws[0].ToString()}");
-                        }
-
-                        if (throws.Count >= 2)
-                        {
-                            Text.Write($"Throw 2: {throws[1].ToString()}");
-                            Text.Write("");
-
-                            (double x, double z) = MinecraftStrongHoldCalculator.Find(throws[throws.Count - 2], throws[throws.Count - 1]);
-                            Text.Write($"Stronghold: X: {Math.Round(x)} Z: {Math.Round(z)}", ConsoleColor.Green);
-
-                            throws.RemoveAt(0);
-                        }
+                        HandleOverworldCommand(clipboardString);
                     }
-                    catch (Exception ex)
+                    else if (clipboardString.Contains("minecraft:the_nether"))
                     {
-                        Text.Write("Detected F3+C command on clipboard but it failed to parse.", ConsoleColor.Red);
-
-                        if (verbose)
-                        {
-                            Text.Write(ex.ToString(), ConsoleColor.DarkRed);
-                        }
+                        HandleNetherCommand(clipboardString);
                     }
                 }
 
@@ -92,6 +71,82 @@ public class Program
             }
 
             Thread.Sleep(250);
+        }
+    }
+
+    private static void HandleOverworldCommand(string command)
+    {
+        try
+        {
+            throws.Add(MinecraftCommandParser.PointFromCommand(command));
+            Text.Clear();
+
+            if (throws.Count >= 1)
+            {
+                Text.Write($"Throw 1: {throws[0].ToString()}");
+            }
+
+            if (throws.Count >= 2)
+            {
+                Text.Write($"Throw 2: {throws[1].ToString()}");
+                Text.Write("");
+
+                (double x, double z) = MinecraftStrongHoldCalculator.Find(throws[throws.Count - 2], throws[throws.Count - 1]);
+                Text.Write($"Stronghold: X: {Math.Round(x)} Z: {Math.Round(z)}", ConsoleColor.Green);
+
+                throws.RemoveAt(0);
+            }
+        }
+        catch (Exception ex)
+        {
+            Text.Write("Detected F3+C command on clipboard but it failed to parse.", ConsoleColor.Red);
+
+            if (verbose)
+            {
+                Text.Write(ex.ToString(), ConsoleColor.DarkRed);
+            }
+        }
+    }
+
+    private static void HandleNetherCommand(string command)
+    {
+        try
+        {
+            throws.Clear();
+            Text.Clear();
+
+            if (netherPortalPoint == null)
+            {
+                netherPortalPoint = MinecraftCommandParser.PointFromCommand(command);
+                Text.Write($"Nether coordinates: {netherPortalPoint.ToString()}", ConsoleColor.Green);
+            }
+            else
+            {
+                Point currentPoint = MinecraftCommandParser.PointFromCommand(command);
+
+                double angle = (Math.Atan2(currentPoint.x - netherPortalPoint.x, currentPoint.z - netherPortalPoint.z));
+                angle = (-(angle / Math.PI) * 360.0d) / 2.0 + 180.0;
+
+                if (angle > 180)
+                {
+                    angle = -180 + (angle - 180);
+                }
+
+                int heightDifference = (int)Math.Round(netherPortalPoint.y - currentPoint.y);
+
+                Text.Write($"Portal coordinates: {netherPortalPoint.ToString()}", ConsoleColor.Green);
+                Text.Write($"Angle to portal: {Math.Round(angle, 1)}");
+                Text.Write($"Portal is {Math.Abs(heightDifference)}{(heightDifference > 0 ? "▲ above" : "▼ below")} you.");
+            }
+        }
+        catch (Exception ex)
+        {
+            Text.Write("Detected F3+C command on clipboard but it failed to parse.", ConsoleColor.Red);
+
+            if (verbose)
+            {
+                Text.Write(ex.ToString(), ConsoleColor.DarkRed);
+            }
         }
     }
 }
