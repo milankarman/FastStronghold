68f74713ce43e1a6ee883356135f3754360fe99d
Milan Karman <milankarman@gmail.com>
2020-11-07 03:49:06 +0100

    Removed accidental extra time writing nether travel, added comments and changed nether travel suggestion method name

--- src/Program.cs
+++ src/Program.cs
@@ -74,7 +74,7 @@ public class Program
                         Process.Start("explorer.exe", "https://github.com/milankarman/fast-stronghold#usage");
                         Text.Update();
                         break;
-                        
+
                     default:
                         Text.Update();
                         break;
@@ -135,18 +135,13 @@ public class Program
             if (throws.Count >= 1)
             {
                 Text.Write($"Throw 1: {throws[0].ToString()}");
-
-                // Calculate where our current angle hits the average stronghold distance for nether travel
-                (double x, double z) = TrigonometryCalculator.GetLineIntersectionOnCircle(throws[0]);
-
-                Text.Write($"Suggested nether travel location: X:{Math.Round(x)}, Z:{Math.Round(z)}");
             }
 
             // If we have only done a single throw and no more, suggest nether travel coordinates
             if (throws.Count == 1)
             {
                 // Calculate where our current angle hits the average stronghold distance for nether travel
-                (double x, double z) = TrigonometryCalculator.GetLineIntersectionOnCircle(throws[0]);
+                (double x, double z) = TrigonometryCalculator.GetLineIntersectionOnCircle(throws[0], 216);
                 Text.Write($"Suggested nether travel location: X:{Math.Round(x)}, Z:{Math.Round(z)}");
             }
 
--- src/math/TrigonometryCalculator.cs
+++ src/math/TrigonometryCalculator.cs
@@ -11,6 +11,7 @@ public static class TrigonometryCalculator
         return (x, z);
     }
 
+    // Gets the angle from one point to the next
     public static double GetAngleAToB(Point a, Point b)
     {
         double angle = (Math.Atan2(a.x - b.x, a.z - b.z));
@@ -24,6 +25,7 @@ public static class TrigonometryCalculator
         return angle;
     }
 
+    // Gets the distance from one point to another
     public static double GetDistanceBetweenPoints(Point a, Point b)
     {
         double xDistance = a.x - b.x;
@@ -35,8 +37,9 @@ public static class TrigonometryCalculator
         return distance;
     }
 
+    // Takes a point and angle and returns the coordinates of where it intersects with a circle of the given radius
     // This formula is thanks to Sharpieman20 (https://github.com/Sharpieman20/Sharpies-Speedrunning-Tools)
-    public static (double, double) GetLineIntersectionOnCircle(Point point)
+    public static (double, double) GetLineIntersectionOnCircle(Point point, double radius)
     {
         double x = point.x;
         double z = point.z;
@@ -49,7 +52,6 @@ public static class TrigonometryCalculator
 
         angle -= 180;
 
-        double o = 216;
         double d = 90 - angle;
 
         double x1 = x / 8;
@@ -60,7 +62,7 @@ public static class TrigonometryCalculator
         double a = 1 + (m1 * m1);
         double b1 = -1 * m1 * x1 + z1;
         double b = 2 * m1 * b1;
-        double co = b1 * b1 - o * o;
+        double co = b1 * b1 - radius * radius;
 
         double xp = ((-1 * b) + (Math.Sign(angle) * Math.Sqrt(b * b -4 * a * co))) / (2 * a);
         double zp = m1 * xp + b1;
